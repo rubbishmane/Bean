aa2f7aef315faed56cb03b53d407acc746385dde
MatteyoMcGowan <159955788+MatteyoMcGowan@users.noreply.github.com>
2024-05-30 10:23:21 +1200

    MainMenu
    now Changes scene

---
+++ Assets/TextMesh Pro/Scripts/Health.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Alteruna;
+
+//Written Entirely by Matthew
+public class Health : AttributesSync
+{
+    [SerializeField] private int maxHealth = 100;
+    //SyncField makes the var synch with multiplayer
+    [SynchronizableField] public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L)){TakeDamage(10);}
+        if(Input.GetKeyDown(KeyCode.K)){Heal(10);}
+
+    }
+    public void ReDoBroadcast(){
+        BroadcastRemoteMethod("TakeDamage", 20);
+    }
+    [SynchronizableMethod]
+    public void TakeDamage (int amount)
+    {
+        currentHealth -=  amount; 
+        if (currentHealth <= 0) 
+        {
+            currentHealth = 0;  
+            Die();
+        }
+    } 
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    private void Die()
+    {
+        //Implemeent what happens when you die
+        Debug.Log("You have died tragically");
+        
+        //indexUser = Alteruna.Avatar.Possessor.Index;
+        //Alteruna.KickUser(indexUser);
+        Application.Quit();
+    }
+
+    public int GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+
+    public int GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+}
