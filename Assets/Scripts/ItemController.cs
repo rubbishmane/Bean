654dcfb60d1c0f2cf96856e7c1a63ba88281967a
rubbishmane <107376193+rubbishmane@users.noreply.github.com>
2024-07-22 18:23:14 +1200

    gun

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -8,14 +8,47 @@ public class ItemController : MonoBehaviour
     public GameObject[] items;
     void Start()
     {
-        
+         for (int i = 0; i <= items.Length; i++)
+        {
+            items[i].SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        string n = Input.inputString();
-        for (int i; i = 0; i++;)
-        
+        string s = Input.inputString;
+        switch (s)
+        {
+            case "Alpha1":
+                ClearEquipt();
+                items[0].SetActive(true);
+                break;
+            case "Alpha2":
+                ClearEquipt();
+                items[1].SetActive(true);
+                break;
+            case "Alpha3":
+                ClearEquipt();
+                items[2].SetActive(true);
+                break;
+            case "Alpha4":
+                ClearEquipt();
+                items[3].SetActive(true);
+                break;
+            case "Alpha5":
+                ClearEquipt();
+                items[4].SetActive(true);
+                break;    
+        }    
     }
+
+    public void ClearEquipt()
+    {
+        for (int i = 0; i <= items.Length; i++)
+        {
+            items[i].SetActive(false);
+        }
+    }
+    
 }
