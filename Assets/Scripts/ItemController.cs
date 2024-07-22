0f24464029e608b1cccddf2fb7af11f5d75f4614
rubbishmane <107376193+rubbishmane@users.noreply.github.com>
2024-07-22 16:01:39 +1200

    ItemController

--- Assets/Scripts/ItemController.cs
+++ Assets/Scripts/ItemController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ItemController : MonoBehaviour
 {
     
+    public GameObject[] items;
     void Start()
     {
         
@@ -13,6 +14,8 @@ public class ItemController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string n = Input.inputString();
+        for (int i; i = 0; i++;)
         
     }
 }
