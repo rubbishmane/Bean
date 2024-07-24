ff4e85d390e5f34280cccd85c558fe989be66e2c
MatteyoMcGowan <mattey1551@gmail.com>
2024-07-24 22:13:32 +1200

    Improved MainMenu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -6,27 +6,58 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject startMenuPanel;
     public GameObject optionsMenuPanel;
+    public GameObject createOrChoosePanel;
+    public GameObject viewLobbyListPanel;
+    public GameObject createGamePanel;
 
     void Start()
     {
         ShowStartMenu();
     }
 
-    public void PlayGame ()
-    {
-        SceneManager.LoadScene("LobbyFinder");
-    }
-
     public void ShowStartMenu()
     {
         startMenuPanel.SetActive(true);
         optionsMenuPanel.SetActive(false);
+        createOrChoosePanel.SetActive(false);
+        viewLobbyListPanel.SetActive(false);
+        createGamePanel.SetActive(false);
+
     }
 
     public void ShowOptionsMenu()
     {
         startMenuPanel.SetActive(false);
         optionsMenuPanel.SetActive(true);
+        createOrChoosePanel.SetActive(false);
+        viewLobbyListPanel.SetActive(false);
+        createGamePanel.SetActive(false);
+    }
+
+    public void ShowCreateOrChooseMenu() 
+    {
+        startMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        createOrChoosePanel.SetActive(true);
+        viewLobbyListPanel.SetActive(false);
+        createGamePanel.SetActive(false);
+    }
+
+   public void ShowLobbyListMenu() 
+    {
+        startMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        createOrChoosePanel.SetActive(false);
+        viewLobbyListPanel.SetActive(true);
+        createGamePanel.SetActive(false);
+    }
+       public void ShowCreateGameMenu() 
+    {
+        startMenuPanel.SetActive(false);
+        optionsMenuPanel.SetActive(false);
+        createOrChoosePanel.SetActive(false);
+        viewLobbyListPanel.SetActive(false);
+        createGamePanel.SetActive(true);
     }
 
     public void OnStartGame()
