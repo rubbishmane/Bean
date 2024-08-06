00b85ffc056f1d2a3b1bef66973abdf2f48fd965
MatteyoMcGowan <159955788+MatteyoMcGowan@users.noreply.github.com>
2024-08-07 10:30:13 +1200

    Materials

---
+++ Assets/Scripts/HealAndBoost.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealAndBoost : MonoBehaviour
+{
+    public float speedMulti;
+
+    public bool isInstant;
+}
+
+
