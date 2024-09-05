6113cb4227eeec517698486c25b76dc2e8c028a7
MatteyoMcGowan <159955788+MatteyoMcGowan@users.noreply.github.com>
2024-09-06 09:29:31 +1200

    UpdatedHealthBar

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class Healthbar : MonoBehaviour
 {
     public Slider slider;
-    
+    public Health healthScript; 
+
+    void Start()
+    {
+        SetMaxHealth(healthScript.health);
+    }
+
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
@@ -16,8 +21,12 @@ public class Healthbar : MonoBehaviour
 
     public void SetHealth(float health)
     {
-
         slider.value = health;
     }
 
+    void Update()
+    {
+        // Update the health bar based on the current health value in the Health script
+        SetHealth(healthScript.health);
+    }
 }
