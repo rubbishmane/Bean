24221cec9ea1c8d083033377d8413b7799315f8b
rubbishmane <107376193+rubbishmane@users.noreply.github.com>
2024-09-11 13:24:36 +1200

    bombaclat

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Alteruna;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : AttributesSync
@@ -13,12 +14,33 @@ public class GameManager : AttributesSync
     int bombCount;
     User winner;
     
+    Transform[] bombPosititons;
+    [SerializeField] GameObject bombPrefab;
+    Bomb[] bomb;
+    
     // Start is called before the first frame update
     void Start()
     {
         spawningScript = GameObject.Find("Spawning Manager").GetComponent<SpawnPlayers>();
+
+        SetBombLocs();
+    }
+    void SetBombLocs()
+    {
+        for(int i = 0; i < bomb.Length; i++)
+        {
+            int bombIndex = UnityEngine.Random.Range(0, 3);
+            Transform loc = bomb[i].location[bombIndex].transform;
+            SpawnBombs(loc);
+            
+        }
+        
     }
 
+    void SpawnBombs(Transform where)
+    {
+        Instantiate(bombPrefab, where);
+    }
     
     void Update()
     {
@@ -45,6 +67,13 @@ public class GameManager : AttributesSync
             Multiplayer.LoadScene("Defense Wins");
        }
        
+    }
+
+    public class Bomb : GameManager
+    {
+        int roomIndex;
+        public GameObject[] location;
+        
     }
 }
 
